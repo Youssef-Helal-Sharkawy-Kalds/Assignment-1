329a1c9578d88857f3a64949791e670a7e5b837e
Youssef-Helal-Sharkawy-Kalds <youssef.helal.dev@gmail.com>
2025-02-22 18:29:31 +0200

    get all books and add new book endpoints are done!

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ app.get("/", (req, res) => {
     res.send("Web Applications Development - Spring 2025: Assignment 1");
 });
 
+app.use(express.json());
+
+let books = [
+    { id: 1, title: "Book 1", author: "Book 1 Author" },
+    { id: 2, title: "Book 2", author: "Book 2 Author" },
+    { id: 3, title: "Book 3", author: "Book 3 Author" },
+];
+
+// get all books
+app.get("/books", (req, res) => {
+    res.json(books);
+});
+
+// add a new book
+app.post("/books", (req, res) => {
+    const newBook = {
+        id: books.length + 1,
+        title: req.body.title,
+        author: req.body.author,
+    };
+    books.push(newBook);
+    res.status(201).json(newBook);
+});
+
 app.listen(port, () => {
     console.log(`The app listening on http://localhost:${port}`);
 });
